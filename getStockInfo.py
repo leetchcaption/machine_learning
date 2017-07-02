e0edae7ad2b0ba20a8a69a3b1a9d0346c3c87ae4
lyric <leetch@126.com>
2017-07-02 13:07:59 +0800

    getStockInfo.py

---
+++ getStockInfo.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Get quotesdf
+
+@author: Dazhuang
+"""
+
+import requests
+import re
+import json
+import pandas as pd
+from datetime import date
+import csv
+
+
+def retrieve_quotes_historical(stock_code):
+    quotes = []
+    url = 'https://finance.yahoo.com/quote/%s/history?p=%s' % (stock_code, stock_code)
+    r = requests.get(url)
+    m = re.findall('"HistoricalPriceStore":{"prices":(.*),"isPending"', r.text)
+    if m:
+        quotes = json.loads(m[0])
+        quotes = quotes[::-1]
+    return [item for item in quotes if not 'type' in item]
+
+quotes = retrieve_quotes_historical('IBM')
+list1 = []
+for i in range(len(quotes)):
+    x = date.fromtimestamp(quotes[i]['date'])
+    y = date.strftime(x, '%Y-%m-%d')
+    list1.append(y)
+quotesdf_ori = pd.DataFrame(quotes, index=list1)
+quotesdf_m = quotesdf_ori.drop(['unadjclose'], axis=1)
+quotesdf = quotesdf_m.drop(['date'], axis=1)
+# with open('quotesdf.csv','rb'):
+
+
+print(quotesdf)
+
